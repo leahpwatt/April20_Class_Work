f6c58cddb6df06de5756e407445252c0a80573dc
Leah <leahpwatt@gmail.com>
2016-04-21 18:16:28 -0400

    lab 8 shopping list

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var groceryList = [
 		{ 
-			name: "strawberries",
+			name: "Strawberries",
 			price: 2.99
 		},
-		{	name: "yogurt",
+		{	name: "Yogurt",
 			price: 3.49
 		},
-		{	name: "juice",
+		{	name: "Juice",
 			price: 1.99
 		},
-		{	name: "bagels",
+		{	name: "Bagels",
 			price: 2.49
 		}
 	];
@@ -17,15 +17,26 @@ var groceryList = [
 var total = 0;
 var list = document.getElementById("list");
 var foodTotal = document.getElementById("foodTotal");
+var button = document.getElementById("button");
 
 groceryList.forEach(function(item){
-var newItem = document.createElement("li");
-newItem.innerText += item.name + ": " + item.price;
-list.appendChild(newItem);
-total += item.price;
-});
+	var newItem = document.createElement("li");
+	newItem.innerText += item.name + " - " + item.price;
+	list.appendChild(newItem);
+	total += item.price;
+	});
 
+button.addEventListener("click", function(){
+	var userFood = document.getElementById("food").value;
+	var userPrice = document.getElementById("price").value;
+	var newUserFood = document.createElement("li");
+	newUserFood.innerText += userFood + " - " + userPrice;
+	list.appendChild(newUserFood);
+	total += parseFloat(userPrice, 10);
 	foodTotal.innerText = "The total is " + total + ("!");
+	});
+
+
 
 
 
